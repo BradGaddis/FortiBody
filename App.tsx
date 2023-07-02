54e7fa2ac97a4cd0be9bf177dbfc71e0b18d2137
brad gaddis <bradleygad@gmail.com>
2023-07-02 15:42:59 -0400

    fix camel case

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavigationContainer,useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { GeneralExercises } from './GeneralExercises';
+import { GeneralExercises } from './Exercise/GeneralExercises';
 import { total_exercises_dict } from './exercise_store';
 import { Exercise } from './Exercise/Exercise';
 import  Home  from './Home';
@@ -29,7 +29,7 @@ export default function App() {
 
 export const generate_exercise_screens = () => {
   return total_exercises_dict.map((exercise : any) => (
-    <Stack.Screen key={exercise.id} name={exercise.name}>
+    <Stack.Screen key={exercise.id} name={exercise.name} >
       {() => <Exercise name={exercise.name} />}
     </Stack.Screen>
   ))
