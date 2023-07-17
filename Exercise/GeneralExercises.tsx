23e023238db254a3837de1a3a60d6fcb855899af
brad gaddis <bradleygad@gmail.com>
2023-07-17 11:55:47 -0400

    update styles

--- Exercise/GeneralExercises.tsx
+++ Exercise/GeneralExercises.tsx
@@ -1,44 +1,43 @@
 import {  Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { powerlifting_exercises, total_exercises_dict } from './exercise_store';
 import React from 'react';
+import styles from '../Styles';
 
 export function GeneralExercises({ navigation } : { navigation: any }) {
     return (
-      <View>
-        <Text>All Exercises Available</Text>
+      <View style={styles.generalExerciseContainer}>
+        <Text>All Exercises Available At This Time</Text>
         <ScrollView>
             <ShowExercisesGeneral exercises={total_exercises_dict} navigation={navigation}/>
         </ScrollView>
       </View>
     );
   }
-
-
   
-  function ShowExercisesGeneral({ exercises, navigation }) {
-    return (
-      <View style={{
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-      }}>
-        {
-          exercises.map((exercise : any) => (
-            <TouchableOpacity
-              key={exercise.id}
-              onPress={() => navigation.navigate(exercise.name)}
-            >
-              <Image
-                source={exercise.img}
-                style={{ width: 200, height: 200 }}
-              />
-              <Text>{exercise.name}</Text>
-            </TouchableOpacity>
-          ))
-        }
-      </View>
-    )
-    
-    
-  } 
+function ShowExercisesGeneral({ exercises, navigation }) {
+  return (
+    <View style={
+      styles.showGeneralExerciseContainer
+            }>
+      {
+        exercises.map((exercise : any) => (
+          <TouchableOpacity
+            key={exercise.id}
+            onPress={() => navigation.navigate(exercise.name)}
+            style={styles.generalExerciseButton}
+          >
+            <Image
+              source={exercise.img}
+              style={styles.generalExerciseImage}
+            />
+            <Text style={
+              styles.generalExerciseText
+            }>{exercise.name}</Text>
+          </TouchableOpacity>
+        ))
+      }
+    </View>
+  )
+  
+  
+} 
