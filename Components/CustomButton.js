b3c2af0c8817253d8c486e8f4d5762fe4b90a99b
brad gaddis <bradleygad@gmail.com>
2023-07-24 18:31:33 -0400

    add radial button

--- Components/CustomButton.js
+++ Components/CustomButton.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { TouchableOpacity, Text, Animated } from "react-native";
 
 
 function CustomButton({style = null, title, onPress = null, textColor = "#fff"}) {
@@ -13,6 +13,53 @@ function CustomButton({style = null, title, onPress = null, textColor = "#fff"})
             
     return (
         <TouchableOpacity style={[defaultStyle, style]} onPress={onPress}>
+            <Text style={{color: textColor}}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export function RadialPulsatingButton({ title, onPress = null, textColor = "#fff"}) {
+    const defaultStyle = {
+        backgroundColor: "#668cff",
+        padding: 10,
+        margin: 10,
+        borderRadius: 50,
+        alignItems: "center",
+    }
+
+    const animation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+      const animationLoop = Animated.loop(
+        Animated.sequence([
+          Animated.timing(animation, {
+            toValue: 1,
+            duration: 750,
+            useNativeDriver: true,
+          }),
+          Animated.timing(animation, {
+            toValue: 0,
+            duration: 750,
+            useNativeDriver: true,
+          }),
+        ])
+      );
+  
+      animationLoop.start();
+  
+      return () => animationLoop.stop();
+    }, []);
+  
+    const buttonSize = animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [50, 60],
+    });
+  
+    return (
+      <TouchableOpacity
+        style={[defaultStyle, { width: buttonSize, height: buttonSize }]}
+        onPress={onPress}
+        >
             <Text style={{color: textColor}}>{title}</Text>
         </TouchableOpacity>
     )
